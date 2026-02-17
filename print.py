c5100dd6fef3d9e8536c8193ab1de2fc146c6971
Pari9678 <vanillap1711@gmail.com>
2026-02-17 20:49:21 +1100

    Add files via upload

---
+++ print.py
@@ -0,0 +1,4 @@
+print ("Welcome to the world of programming")
+print ("Hello world")
+print (10) 
+print ("Hi" , 10 , 2.5)
---
+++ type_function.py
@@ -0,0 +1,22 @@
+a = 20
+b = 2.1
+c = False
+d = "hello"
+
+type(a) 
+type(b)
+type(c)
+type(d)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+type(20)
+type(2.1)
+type(False)
+type("hello")
+print(type(20))
+print(type(2.1))
+print(type(False))
+print(type("hello"))

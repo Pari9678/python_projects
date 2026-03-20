991f035349c5681d4446b78433af9f5789a14d88
Pari9678 <vanillap1711@gmail.com>
2026-03-20 21:38:39 +1100

    Add files via upload

---
+++ asciivalue.py
@@ -0,0 +1,19 @@
+print("ASCII Value Checker")
+char = input("Enter any single character: ")
+if type(char) is str and len(char) == 1:
+    ascii_val = ord(char)
+    print(f"\nCharacter: '{char}'")
+    print(f"ASCII Value: {ascii_val}")
+    print("\nCharacter Type: ", end="")
+    if ascii_val >= 48 and ascii_val <= 57:
+        print("digit")
+    elif ascii_val >= 65 and ascii_val <= 90:
+        print("uppercase Letter")
+    elif ascii_val >= 97 and ascii_val <= 122:
+        print("lowercase Letter")
+    elif ascii_val == 32:
+        print("Space")
+    else:
+        print("Special Character")
+else:
+    print("\nError: Please enter exactly ONE character!")

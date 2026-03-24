17a20abf052505a89d704332cda556bfe362e8e1
Pari9678 <vanillap1711@gmail.com>
2026-03-24 21:29:59 +1100

    Add files via upload

---
+++ binaryconversion.py
@@ -0,0 +1,11 @@
+num = float(input("Enter a decimal number: "))
+binary = ""
+while num > 0:
+    remainder = num % 2
+    binary = str(remainder) + binary
+    num = num // 2
+
+if binary == "":
+    binary = "0"
+
+print("Binary:", binary)
